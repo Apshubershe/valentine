5994ad38ecab8ea666912c1cccd5f5524b43ba81
Apshubershe <kanalyoukap@gmail.com>
2026-02-04 19:22:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-const noBtn = document.getElementById("noBtn");
+const sound = document.getElementById("sound");
 
-// начальная позиция кнопки "Нет"
-noBtn.style.top = "55%";
-noBtn.style.left = "55%";
+const step1 = document.getElementById("step1");
+const step2 = document.getElementById("step2");
+const step3 = document.getElementById("step3");
 
-// функция случайного перемещения по экрану
-function moveNoButton() {
-  const padding = 20;
+const no1 = document.getElementById("no1");
+const no2 = document.getElementById("no2");
 
-  const maxX = window.innerWidth - noBtn.offsetWidth - padding;
-  const maxY = window.innerHeight - noBtn.offsetHeight - padding;
+function runAway(btn) {
+  const maxX = window.innerWidth - btn.offsetWidth;
+  const maxY = window.innerHeight - btn.offsetHeight;
 
-  const x = Math.random() * maxX;
-  const y = Math.random() * maxY;
-
-  noBtn.style.left = x + "px";
-  noBtn.style.top = y + "px";
+  btn.style.left = Math.random() * maxX + "px";
+  btn.style.top = Math.random() * maxY + "px";
 }
 
-// убегает при наведении и клике
-noBtn.addEventListener("mouseover", moveNoButton);
-noBtn.addEventListener("click", moveNoButton);
+no1.onclick = () => runAway(no1);
+no2.onclick = () => runAway(no2);
+
+document.getElementById("yes1").onclick = () => {
+  sound.currentTime = 0;
+  sound.play().catch(()=>{});
+
+  step1.classList.add("hidden");
+  step2.classList.remove("hidden");
+};
+
+document.getElementById("yes2").onclick = () => {
+  step2.classList.add("hidden");
+  step3.classList.remove("hidden");
+};
